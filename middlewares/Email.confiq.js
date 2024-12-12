5a62bff4fd9683b70cf4d5d94b034edafa95c6d1
Utkarsh Goyal <utkarshgoyal07@gmail.com>
2024-12-12 23:24:03 +0530

    email confin updated

--- middlewares/Email.confiq.js
+++ middlewares/Email.confiq.js
@@ -2,9 +2,9 @@ import nodemailer from 'nodemailer'
 
 export const transporter = nodemailer.createTransport({
   service: 'Gmail',
-  //   host: "smtp.gmail.com",
-  // port: 587,
-  // secure: false, // true for port 465, false for other ports
+  host: 'smtp.yourprovider.com', // e.g., smtp.gmail.com
+    port: 465, // Secure port (SSL)
+    secure: true, // true for port 465, false for other ports
   auth: {
     user: `${process.env.EMAIL_USER_NAME}` ,
     pass: `${process.env.EMAIL_PASS}`,
